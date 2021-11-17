dd5dd150644cd6c552f08729961057de677dca88
JanLetovanec <janko.let@gmail.com>
2021-11-17 02:30:59 +0000

    Messing around in main

--- src/uk/ac/jl2119/partII/Main.java
+++ src/uk/ac/jl2119/partII/Main.java
@@ -1,7 +1,10 @@
 package uk.ac.jl2119.partII;
 
+import uk.ac.jl2119.partII.UEF.UEFDecoder;
 import uk.ac.jl2119.partII.UEF.UEFEncoder;
+import uk.ac.jl2119.partII.WavManipulation.AbstractReaderFactory;
 import uk.ac.jl2119.partII.WavManipulation.AbstractWriterFactory;
+import uk.ac.jl2119.partII.WavManipulation.WavReaderFactory;
 import uk.ac.jl2119.partII.WavManipulation.WavWriterFactory;
 import uk.ac.thirdParty.WavFile.WavFileException;
 
@@ -11,17 +14,21 @@ public class Main {
 
     public static void main(String[] args) throws IOException, WavFileException {
         final int SAMPLE_RATE = 44100;
+        final double BASE_FREQUENCY = 1200;
+        final String FILE_NAME = "./output/Module3/test.wav";
 
         String data = "Hi this is a string with some length, right? " +
-                "Hopefully, this will not last like nothing..." +
-                "Hopefully, this will not last like nothing..." +
-                "Hopefully, this will not last like nothing..." +
-                "Hopefully, this will not last like nothing..." +
                 "Hopefully, this will not last like nothing...";
+        System.out.println(data);
 
-        AbstractWriterFactory factory = new WavWriterFactory("./output/Module2/test.wav", SAMPLE_RATE);
-        Encoder encoder = new UEFEncoder(factory, false);
-
+        AbstractWriterFactory writerFactory = new WavWriterFactory(FILE_NAME, SAMPLE_RATE);
+        UEFEncoder encoder = new UEFEncoder(writerFactory, false);
         encoder.generateSignal(data.getBytes());
+
+        AbstractReaderFactory readerFactory = new WavReaderFactory(FILE_NAME);
+        UEFDecoder decoder = new UEFDecoder(readerFactory, false);
+        byte[] transmittedData = decoder.decodeSignal();
+
+        System.out.println(new String(transmittedData));
     }
 }
