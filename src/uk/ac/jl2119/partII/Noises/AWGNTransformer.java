af62caf9615dd4ac635f61a99dc6e1ad59432dc4
JanLetovanec <janko.let@gmail.com>
2021-12-16 15:07:00 +0000

    awgn boilerplate

---
+++ src/uk/ac/jl2119/partII/Noises/AWGNTransformer.java
@@ -0,0 +1,11 @@
+package uk.ac.jl2119.partII.Noises;
+
+
+import uk.ac.jl2119.partII.ITransformer;
+
+public class AWGNTransformer implements ITransformer<Double, Double> {
+    @Override
+    public Double[] transform(Double[] input) {
+        return new Double[0];
+    }
+}
