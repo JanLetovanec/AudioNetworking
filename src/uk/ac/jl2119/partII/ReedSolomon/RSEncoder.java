aa92d02f4b84a30a02fcdb09fda2df91c6d42685
JanLetovanec <janko.let@gmail.com>
2022-02-15 00:27:59 +0000

    Removed unused constructor logic

--- src/uk/ac/jl2119/partII/ReedSolomon/RSEncoder.java
+++ src/uk/ac/jl2119/partII/ReedSolomon/RSEncoder.java
@@ -1,6 +1,5 @@
 package uk.ac.jl2119.partII.ReedSolomon;
 
-import org.ejml.simple.SimpleMatrix;
 import uk.ac.jl2119.partII.ITransformer;
 
 import java.util.Arrays;
@@ -11,28 +10,18 @@ import static uk.ac.jl2119.partII.utils.StreamUtils.partitionData;
 public class RSEncoder implements ITransformer<Byte, Byte> {
     private static final int BLOCK_SIZE = 255;
     private static final int DATA_SIZE = 223;
-
-    private final SimpleMatrix generator;
+    private static final int SHIFT = 1;
 
     public RSEncoder() {
-        double[][] matrixData = new double[BLOCK_SIZE][DATA_SIZE];
-        for(int row = 0; row < matrixData.length; row++) {
-            for(int column = 0; column < matrixData[row].length; column++) {
-                matrixData[row][column] = Math.pow(row, column);
-            }
-        }
-        generator = new SimpleMatrix(matrixData);
     }
 
     @Override
     public Byte[] transform(Byte[] input) {
-        input = padData(input, DATA_SIZE);
-        Byte[] output = partitionData(input, DATA_SIZE).stream()
+        Byte[] paddedData = padData(input, DATA_SIZE);
+        return partitionData(paddedData, DATA_SIZE).stream()
                 .map(x -> x.toArray(Byte[]::new))
                 .flatMap(x -> Arrays.stream(transformBlock(x)))
                 .toArray(Byte[]::new);
-
-        return output;
     }
 
     private Byte[] transformBlock(Byte[] blockData) {
@@ -47,14 +36,12 @@ public class RSEncoder implements ITransformer<Byte, Byte> {
     private Polynomial getMessagePoly(Byte[] data) {
         data = padData(data, BLOCK_SIZE);
         FiniteFieldElement[] coefficients = Arrays.stream(data)
-                .map(x -> new FiniteFieldElement(x))
+                .map(FiniteFieldElement::new)
                 .toArray(FiniteFieldElement[]::new);
         return new Polynomial(coefficients);
     }
 
     private Polynomial getGeneratorPoly() {
-        final int SHIFT = 1;
-
         Polynomial genPoly = new Polynomial(new FiniteFieldElement[]{FiniteFieldElement.getOne()});
         for (int i = 0; i < BLOCK_SIZE - DATA_SIZE; i++) {
             // term = (x + alpha^i)
