5d8441a1f5e75f3e10230a6b1082dce7f7d96155
JanLetovanec <janko.let@gmail.com>
2022-02-15 23:39:03 +0000

    Added BM algo - finding error locator

--- src/uk/ac/jl2119/partII/ReedSolomon/RSDecoder.java
+++ src/uk/ac/jl2119/partII/ReedSolomon/RSDecoder.java
@@ -67,4 +67,100 @@ public class RSDecoder implements ITransformer<Byte, Byte> {
         StreamUtils.copyBytesIn(targetData, rawData, 0, DATA_SIZE);
         return targetData;
     }
+
+    /**
+     * Find Lambda(x) = Product(x*alpha^j - 1), where j is error location
+     * Do this by using **Berlekamp–Massey** algorithm:
+     * https://en.wikipedia.org/wiki/Berlekamp%E2%80%93Massey_algorithm
+     * @param syndromes -- syndromes of the message
+     * @return -- the Lambda(x) polynomial
+     */
+    private Polynomial getErrorLocPoly(FiniteFieldElement[] syndromes) {
+        // Berkley-Massey algo
+        // Initialize
+        Polynomial syndromePoly = new Polynomial(syndromes);
+        Polynomial C = getPolyOfOne();  // The current guess poly
+        Polynomial B = getPolyOfOne();  // The last guess poly
+        int L = 0;                      // Estimated errors
+        int m = 1;
+        FiniteFieldElement b = FiniteFieldElement.getOne();// Last discrepancy
+
+        //NOT SURE:
+        // * what direction the syndromes are supposed to go
+
+        for (int n = 0; n < syndromes.length; n++) {
+            // For odd numbers d = 0, so do not even bother
+            if (n % 2 == 1) {
+                continue;
+            }
+
+            // Calculate discrepancy
+            FiniteFieldElement d = getDiscrepancy(syndromes, C,L, n);
+            // if guess is okay, just carry on
+            if (d.isZero()) {
+                m++;
+                continue;
+            }
+            // discrepancies should converge by n = 2L
+            // if its not yet n = 2L
+            // then adjust C and carry on
+            if (2*L > n) {
+                adjustC(C,d, b, m, B);
+                m++;
+                continue;
+            }
+            // if it its 2*L and did not converge yet
+            // adjust L and reset b,B,m
+            Polynomial tempPoly = new Polynomial(C);
+            adjustC(C,d, b, m, B);
+            L = n + 1 - L;
+            B = tempPoly;
+            b = d;
+            m = 1;
+        }
+
+        // If you terminated, C was correct guess
+        return C;
+    }
+
+    private FiniteFieldElement getDiscrepancy(FiniteFieldElement[] syndromes,
+                                              Polynomial C,
+                                              int L,
+                                              int n) {
+        FiniteFieldElement total = FiniteFieldElement.getZero();
+        for (int i = 1; i < L; i++) {
+            total = total.add(C.getCoefficients()[i].multiply(syndromes[n - i]));
+        }
+        return total.add(syndromes[n]);
+    }
+
+    private void adjustC(Polynomial C,
+                         FiniteFieldElement d,
+                         FiniteFieldElement b,
+                         int m,
+                         Polynomial B) {
+        Polynomial temp = new Polynomial(B);
+        FiniteFieldElement scalar = d.divide(b);
+        Polynomial xToM = getXtoM(m);
+        temp.multiply(xToM);
+        temp.multiplyByScalar(scalar);
+
+        C.add(temp);
+    }
+
+    private Polynomial getPolyOfOne() {
+        FiniteFieldElement[] coefs = new FiniteFieldElement[] {
+                FiniteFieldElement.getOne()
+        };
+        return new Polynomial(coefs);
+    }
+
+    private Polynomial getXtoM(int m) {
+        FiniteFieldElement[] elems = new FiniteFieldElement[m + 1];
+        elems = Arrays.stream(elems)
+                .map(x -> FiniteFieldElement.getZero())
+                .toArray(FiniteFieldElement[]::new);
+        elems[0] = FiniteFieldElement.getOne();
+        return new Polynomial(elems);
+    }
 }
