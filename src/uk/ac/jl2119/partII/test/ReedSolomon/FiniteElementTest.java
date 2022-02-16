e3c9fd0e1ea8e4c341c909d99f6130f0ea4f2ad0
JanLetovanec <janko.let@gmail.com>
2022-02-16 15:25:02 +0000

    Addded FF tests

---
+++ src/uk/ac/jl2119/partII/test/ReedSolomon/FiniteElementTest.java
@@ -0,0 +1,43 @@
+package uk.ac.jl2119.partII.test.ReedSolomon;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import uk.ac.jl2119.partII.ReedSolomon.FiniteFieldElement;
+import uk.ac.jl2119.partII.test.GenericTest;
+
+public class FiniteElementTest extends GenericTest{
+    @Test
+    void addSelfIsZero() {
+        FiniteFieldElement a = new FiniteFieldElement((byte) 5);
+        FiniteFieldElement aa = a.add(a);
+        Assertions.assertTrue(aa.isZero());
+    }
+
+    @Test
+    void add4and2Is6() {
+        FiniteFieldElement a = new FiniteFieldElement((byte) 4);
+        FiniteFieldElement b = new FiniteFieldElement((byte) 2);
+        FiniteFieldElement c = a.add(b);
+
+        Assertions.assertEquals(6, c.getValue());
+    }
+
+    @Test
+    void multByZeroIsZero() {
+        FiniteFieldElement a = new FiniteFieldElement((byte) 4);
+        FiniteFieldElement b = new FiniteFieldElement((byte) 0);
+        FiniteFieldElement c = a.multiply(b);
+
+        Assertions.assertEquals(0, c.getValue());
+    }
+
+    @Test
+    void div1By65Is254() {
+        FiniteFieldElement a = new FiniteFieldElement((byte) 1);
+        FiniteFieldElement b = new FiniteFieldElement((byte) 65);
+        FiniteFieldElement c = a.divide(b);
+
+        Assertions.assertEquals(254, c.getValue());
+    }
+
+}
