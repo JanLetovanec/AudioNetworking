cd956f468f6d6d144d5e9c34232e1647bc916426
JanLetovanec <janko.let@gmail.com>
2022-02-17 15:26:58 +0000

    Messing around in main

--- src/uk/ac/jl2119/partII/Main.java
+++ src/uk/ac/jl2119/partII/Main.java
@@ -9,32 +9,53 @@ public class Main {
     static final int SAMPLE_RATE = 44100;
     static final int LENGTH = SAMPLE_RATE * 5;
 
-    static ITransformer<Byte, Double> modulator;
-    static ITransformer<Double, Double> noiseGenerator;
-    static ITransformer<Double, Byte> demodulator;
-    static ITransformer<Double, Double> attenuator;
-
     public static void main(String[] args) {
-        String data = Strings.repeat("AAAAAAAA", 20);
-        Byte[] encoded = new RSEncoder().transform(Boxer.box(data.getBytes()));
-        printStuff(encoded);
+        String a = Strings.repeat("A", 223);
+        String hello = Strings.repeat("HelloWorld", 22).concat("!!!");
+        prettyPrint(a);
+        prettyPrint(hello);
     }
 
     public static String simulate(String data) {
         Byte[] encoded = new RSEncoder().transform(Boxer.box(data.getBytes()));
+        encoded = tamper(encoded);
         Byte[] decoded = new RSDecoder().transform(encoded);
         return new String(Boxer.unBox(decoded));
     }
 
+    public static String tampered(String data) {
+        Byte[] encoded = new RSEncoder().transform(Boxer.box(data.getBytes()));
+        encoded = tamper(encoded);
+        return new String(Boxer.unBox(encoded));
+    }
+
+    private static Byte[] tamper(Byte[] data) {
+        data[50] = 0;
+        data[145] = 17;
+        data[208] = (byte) 250;
+        return data;
+    }
+
     public static void prettyPrint(String data) {
+        String simData = simulate(data);
         System.out.println("IN:");
         System.out.println(data);
+        System.out.println("CORRUPTED:");
+        System.out.println(tampered(data));
         System.out.println("OUT:");
-        System.out.println(simulate(data));
-        System.out.println("Done");
+        System.out.println(simData);
+        System.out.println("===");
+        System.out.println(data.compareTo(simData));
+        System.out.println("::::::::::");
     }
 
     public static void printStuff(Byte[] data) {
         System.out.println(new String(Boxer.unBox(data)));
+    }
+    public static void printStuff(Byte[] data, int finalDigits) {
+        for(int i = data.length - finalDigits; i < data.length; i++) {
+            String toPrint = String.format("%x |", data[i]);
+            System.out.print(toPrint);
+        }
     }
 }
