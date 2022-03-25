71b52b2b58cd2e246285e32f301c64cb76fd97bd
JanLetovanec <janko.let@gmail.com>
2022-03-25 02:50:30 +0000

    Minor refactor

--- src/uk/ac/jl2119/partII/Evaluation/metrics/ErrorRateCalc.java
+++ src/uk/ac/jl2119/partII/Evaluation/metrics/ErrorRateCalc.java
@@ -6,7 +6,7 @@ import uk.ac.jl2119.partII.Evaluation.sims.Simulator;
 public class ErrorRateCalc<P> implements IMetricCalculator<Double, P> {
 
     @Override
-    public Double getMetric(Byte[] input, Simulator sim) {
+    public Double getMetric(Byte[] input, Simulator<P> sim) {
         Byte[] output = sim.getReceivedData(input);
         int correctBits = EvalUtils.getCorrectBits(input, output);
         int totalBits = input.length * 8;   // Cuz counting bits, not bytes!
