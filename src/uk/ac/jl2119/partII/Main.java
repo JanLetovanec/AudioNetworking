ffc3424ecc89537e38ea4023bbee9fbe74c45613
JanLetovanec <janko.let@gmail.com>
2022-03-25 00:11:54 +0000

    Messing around in main

--- src/uk/ac/jl2119/partII/Main.java
+++ src/uk/ac/jl2119/partII/Main.java
@@ -1,8 +1,7 @@
 package uk.ac.jl2119.partII;
 
-import com.google.common.base.Strings;
-import uk.ac.jl2119.partII.ReedSolomon.RSDecoder;
-import uk.ac.jl2119.partII.ReedSolomon.RSEncoder;
+import uk.ac.jl2119.partII.UEF.UEFModulator;
+import uk.ac.jl2119.partII.UEF.UEFSyncDemodulator;
 import uk.ac.jl2119.partII.utils.Boxer;
 
 public class Main {
@@ -10,38 +9,26 @@ public class Main {
     static final int LENGTH = SAMPLE_RATE * 5;
 
     public static void main(String[] args) {
-        String a = Strings.repeat("A", 223);
-        String hello = Strings.repeat("HelloWorld", 22).concat("!!!");
-        prettyPrint(a);
-        prettyPrint(hello);
+//        String a = Strings.repeat("A", 223);
+//        String hello = Strings.repeat("HelloWorld", 22).concat("!!!");
+//        prettyPrint(a);
+//        prettyPrint(hello);
+        Byte[] data = new Byte[]{ 0x02, 0x02, 0x02};
+        Double[] encoded = new UEFModulator(true, SAMPLE_RATE).transform(data);
+        Byte[] decoded = new UEFSyncDemodulator(1200, true, SAMPLE_RATE).transform(encoded);
     }
 
     public static String simulate(String data) {
-        Byte[] encoded = new RSEncoder().transform(Boxer.box(data.getBytes()));
-        encoded = tamper(encoded);
-        Byte[] decoded = new RSDecoder().transform(encoded);
+        Byte[] bData = Boxer.box(data.getBytes());
+        Double[] encoded = new UEFModulator(true, SAMPLE_RATE).transform(bData);
+        Byte[] decoded = new UEFSyncDemodulator(1200, true, SAMPLE_RATE).transform(encoded);
         return new String(Boxer.unBox(decoded));
-    }
-
-    public static String tampered(String data) {
-        Byte[] encoded = new RSEncoder().transform(Boxer.box(data.getBytes()));
-        encoded = tamper(encoded);
-        return new String(Boxer.unBox(encoded));
-    }
-
-    private static Byte[] tamper(Byte[] data) {
-        data[50] = 0;
-        data[145] = 17;
-        data[208] = (byte) 250;
-        return data;
     }
 
     public static void prettyPrint(String data) {
         String simData = simulate(data);
         System.out.println("IN:");
         System.out.println(data);
-        System.out.println("CORRUPTED:");
-        System.out.println(tampered(data));
         System.out.println("OUT:");
         System.out.println(simData);
         System.out.println("===");
